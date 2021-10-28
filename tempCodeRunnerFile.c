0729f99ea1d8c0f6b7b277747c4bfe26c85ff787
Ella Sleptsova <sl.ella@yandex.ru>
2021-10-28 03:00:25 +0300

    5 out of 6

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+typedef struct uint256_t
+{
+    uint64_t n1;
+    uint64_t n2;
+    uint64_t n3;
+    uint64_t n4;
+} uint256_t;
--- uint1024_t.c
+++ uint1024_t.c
@@ -13,29 +13,56 @@
 ввода
 */
 
-typedef struct uint256_t
-{
-    uint64_t n1;
-    uint64_t n2;
-    uint64_t n3;
-    uint64_t n4;
-} uint256_t;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
-typedef union uint1024_t
+/*
+1. uint1024_t from_uint(unsigned int x) - генерация из числа
+2. uint1024_t add_op(uint1024_t x, uint1024_t y) - сложение
+3. uint1024_t subtr_op(uint1024_t x, uint1024_t y) - вычетание
+4. uint1024_t mult_op(uint1024_t x, uint1024_t y) - умножение
+5. void printf_value(uint1024_t x) - вывод в стандартный поток
+вывода
+6. void scanf_value(uint1024_t* x) - чтение из стандартного потока
+ввода
+*/
+ 
+void reverse(char s[])
 {
-    char number[308];
-    struct
-    {
-        uint256_t n1;
-        uint256_t n2;
-        uint256_t n3;
-        uint256_t n4;
-    };
+    int i, j;
+    char c;
+
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void itoa(int n, char s[])
+{
+    int i = 0;
+    do 
+    {       
+        s[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
+    s[i] = '\0';
+    reverse(s);
+}
+
+typedef struct uint1024_t
+{
+    char number[310];
+    unsigned int size;
 } uint1024_t;
 
 uint1024_t from_uint(unsigned int x)
 {
     uint1024_t number;
+
+    number.size = sizeof(x) / sizeof(unsigned int);
     char* c;
     c = (char *)malloc((sizeof(x)/sizeof(unsigned int)) * sizeof(char)); 
     int v = 0; 
@@ -54,82 +81,135 @@ uint1024_t from_uint(unsigned int x)
         c[v - 1 - i] = t;
     }
     strcpy(number.number, c);
+    number.size = v;
     return number;
 }
 
 uint1024_t add_op(uint1024_t x, uint1024_t y)
 {
     uint1024_t number;
-    char sum[307];
+    char sum[310];
     int i;
 
-    for (i=307; i>-1; i--)
+    for (i=309; i>-1; i--)
     {
-        int x2 = x.number[i] + 0;
-        int y2 =  y.number[i] + 0;
-        printf ("x = %d\ty = %d\t", x2,y2);
-        sum[i] = (x2 + y2);
-        printf ("sum[%d] = %c\n", i, sum[i]);
+        int x2 = atoi(&x.number[i]);
+        int y2 =  atoi(&y.number[i]);
+        itoa(x2 + y2, &sum[i]);
     }
-    printf ("sum = %s", sum);
-    for (i = 307; i > -1; i--)
+    strcpy(number.number, sum);
+    return number;
+}
+
+uint1024_t subtr_op(uint1024_t x, uint1024_t y)
+{
+    uint1024_t number;
+    char sum[310];
+    int i;
+    if (x.size > y.size)
     {
-        int x1 = sum[i] + 0;
-        if (x1 / 10 > 0)
+        for (i=309; i>-1; i--)
         {
-            int more = x1 / 10;
-            int y1 = sum[i-1] + 0;
-            y1 += more;
-            sum[i] = y1 +'0';
+            int x2 = atoi(&x.number[i]);
+            int y2 =  atoi(&y.number[i]);
+            if (x2 - y2 < 0)
+            {
+                printf("Unexpexted behaviour");
+                exit (-1);
+            }
+            itoa(x2 - y2, &sum[i]);
+        }
+    }
+    else if (y.size > x.size)
+    {
+        for (i=309; i>-1; i--)
+        {
+            int x2 = atoi(&y.number[i]);
+            int y2 =  atoi(&x.number[i]);
+            if (x2 - y2 < 0)
+            {
+                printf("Unexpexted behaviour");
+                exit (-1);
+            }
+            itoa(x2 - y2, &sum[i]);
+        }
+    }
+    else
+    {
+        int flag = 0;
+        for (i = 0; i < 310; i++)
+        {
+            if (x.number[i] > y.number[i])
+            {
+                flag = 1;
+                break;
+            }
+            else if (y.number[i] > x.number[i])
+            {
+                flag = 2;
+                break;
+            }
+        }
+        if (flag == 1)
+        {
+            for (i=309; i>-1; i--)
+            {
+                int x2 = atoi(&x.number[i]);
+                int y2 =  atoi(&y.number[i]);
+                if (x2 - y2 < 0)
+                {
+                    printf("Unexpexted behaviour");
+                    exit (-1);
+                }
+                itoa(x2 - y2, &sum[i]);
+            }
+        }
+        else
+        {
+            for (i=309; i>-1; i--)
+            {
+                int x2 = atoi(&y.number[i]);
+                int y2 =  atoi(&x.number[i]);
+                if (x2 - y2 < 0)
+                {
+                    printf("Unexpexted behaviour");
+                    exit (-1);
+                }
+                itoa(x2 - y2, &sum[i]);
+            }
         }
     }
     strcpy(number.number, sum);
     return number;
-    /*uint1024_t rez;
-    char sum[308];
-    int x2,y2,ost,more,i;
-    i=307;
-    while (i > 0)
-    {
-        x2 = x.number[i-1] + 0;
-        y2 = y.number[i-1] + 0;
-        ost = ost + (x2 + y2) % 10;
-        more = (x2 + y2) / 10;
-        sum[i] = ost+'0';
-        printf("\n (%c + %c) %d + %d = %d\t%d   %d", x.number[i-1], y.number[i-1], x2, y2, x2+y2,more, ost);
-        printf (" %d %s\n",i ,sum);
-        i--;
-        if (i == 0)
-        {
-            continue;
-        }
-        else
-        {
-            ost = more;
-            more = 0;
-        }
-    }
-    if (i < 0)
-    {
-        sum[i] = more + '0';
-    }
-    strcpy(rez.number, sum);
-    return rez;*/
 }
 
-/*uint1024_t subtr_op(uint1024_t x, uint1024_t y)
-{
-    
-}
+void printf_value(uint1024_t x);
 
 uint1024_t mult_op(uint1024_t x, uint1024_t y)
 {
+    uint1024_t rez, sum;
+    long long summery;
+    char mult[620];
+    int i, j, more;
     
-}*/
+    for (i = 309; i > -1; i--)
+    {
+        for (j = 309; j > -1; j--)
+        {
+            int x2 = atoi(&x.number[i]);
+            int y2 = atoi(&y.number[j]);
+            summery = atoi(&mult[i+j]) + x2 * y2;
+        }
+        
+    }
+    rez = from_uint(summery);
+    return rez;
+}
 
 void scanf_value(uint1024_t *x)
 {
     scanf("%s",x->number);
+    x->size = sizeof(x->number) / (sizeof(char));
 }
 
 void printf_value(uint1024_t x)
@@ -139,10 +219,28 @@ void printf_value(uint1024_t x)
 
 int main(int argc, char *argv[])
 {
-    uint1024_t x,y;
-    x = from_uint(1000);
-    y = from_uint(1000);
-    printf_value(add_op(x,y));
+    uint1024_t number1, number2;
+    unsigned int x;
+    printf("Scanf\n");
+    scanf_value(&number1);
+    scanf_value(&number2);
+    scanf("%u", &x);
+    printf("\n");
+
+    printf("From uint\n");
+    printf_value(from_uint(x));
+    printf("\n");
+
+    printf("Add\n");
+    printf_value(add_op(number1, number2));
+    printf("\n");
+
+    printf("Subtr\n");
+    //printf_value(subtr_op(number1, number2));
+    printf("\n");
+
+    printf("Mult\n");
+    printf_value(mult_op(number1, number2));
     printf("\nDONE");
     return 0;
 }
