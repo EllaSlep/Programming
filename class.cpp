8c3d942274008389d648789313861bd520f694ae
Ella Sleptsova <sl.ella@yandex.ru>
2022-02-14 21:46:48 +0300

    added polygon

--- class.cpp
+++ class.cpp
@@ -11,12 +11,8 @@ class point{
     public:
     dot dot_;
 
-    point(){
-        dot_.x_ = 0;
-        dot_.y_ = 0;
-    }
     //конструктор
-    point (int x, int y){
+    point (int x = 0, int y = 0){
         dot_.x_ = x;
         dot_.y_ = y;
     }
@@ -31,6 +27,23 @@ class point{
         this->dot_.y_ = point_B.dot_.y_;
         return *this;
     }
+    //setters
+    void set_x(const int& x){
+        dot_.x_ = x;
+    }
+    void set_y(const int& y){
+        dot_.y_ = y;
+    }
+    //getters
+    int get_x(const point& point_A){
+        return point_A.dot_.x_;
+    }
+    int get_y(const point& point_B){
+        return point_B.dot_.y_;
+    }
+    //деструктор
+    ~point()
+    {}
 };
 
 class polyline{
@@ -67,11 +80,15 @@ class polyline{
         }
         return rez;
     }
+    //деструктор
+    ~polyline()
+    {}
 };
 
 class closed_polyline{
     public:
     std :: vector <dot> broken_line;
+
     //конструктор
     closed_polyline (int count, ...){
         va_list vl;
@@ -82,7 +99,6 @@ class closed_polyline{
         va_end(vl);
         broken_line.resize(broken_line.size() + 1, broken_line.front()); //broken_line.push_back(broken_line.front());
     }
-
     //конструктор копирования
     closed_polyline(const polyline& polyline_a){
         for (int i = 0; i < polyline_a.broken_line.size(); i++){
@@ -90,7 +106,7 @@ class closed_polyline{
         }
         broken_line.resize(broken_line.size() + 1, broken_line.front());
     }
-    
+
     closed_polyline(const closed_polyline& polyline_b){
         for (int i = 0; i < polyline_b.broken_line.size(); i++){
             broken_line.push_back(polyline_b.broken_line[i]);
@@ -111,6 +127,33 @@ class closed_polyline{
         }
         return rez;
     }
+    //деструктор
+    ~closed_polyline()
+    {}
+};
+
+class polygon : public closed_polyline{
+    public:
+    polygon(const closed_polyline& closed_polyline_a) : closed_polyline(closed_polyline_a) 
+    {}
+    //оператор присваивания
+    polygon& operator= (const polygon& polygon_B){
+        for (int i = 0; i < polygon_B.broken_line.size(); i++){
+            this->broken_line[i] = polygon_B.broken_line[i];
+        }
+        return *this;
+    }
+    //площадь многоугольника
+    double polygon_area(){
+        double rez = 0.0;
+        for (int i = 0; i < broken_line.size() - 1; i++){
+            rez += ((broken_line[i].x_ * broken_line[i + 1].y_) - (broken_line[i].y_ * broken_line[i + 1].x_));
+        }
+        return abs(rez / 2);
+    }
+    //деструктор
+    ~polygon()
+    {}
 };
 
 int main(){
@@ -121,21 +164,27 @@ int main(){
     point B(x, y);
     std :: cin >> x >> y;
     point C(x, y);
-    std :: cout << "\npoints:\n" << "A (" << A.dot_.x_ << ";" << A.dot_.y_ << ")\n" << "B (" << B.dot_.x_ << ";" << B.dot_.y_ << ")\n" << "C (" << C.dot_.x_ << ";" << C.dot_.y_ << ")" << std :: endl;
-    polyline line(3, A.dot_, B.dot_, C.dot_);
-
+    std :: cin >> x >> y;
+    point D(x, y);
+    std :: cout << "\npoints:\nA (" << A.dot_.x_ << ";" << A.dot_.y_ << ")\nB (" << B.dot_.x_ << ";" << B.dot_.y_ << ")\nC (" << C.dot_.x_ << ";" << C.dot_.y_ <<  ")\nD (" << D.dot_.x_ << ";" << D.dot_.y_ << ")" << std :: endl;
+    
+    polyline line(4, A.dot_, B.dot_, C.dot_, D.dot_);
     std :: cout << "\nline length: " << line.length_polyline() << std :: endl;
 
     /* point D(A);
-    std :: cout << "\nA (" << A.dot_.x_ << ";" << A.dot_.y_ << ")\n" << "D (" << D.dot_.x_ << ";" << D.dot_.y_ << ")\n" << std :: endl;
+    std :: cout << "\nA (" << A.dot_.x_ << ";" << A.dot_.y_ << ")\n" << "D (" << D.dot_.x_ << ";" << D.dot_.y_ << ")\n" << std :: endl; */
 
-    point F;
-    F.operator=(A);
-    std :: cout << "\nA (" << A.dot_.x_ << ";" << A.dot_.y_ << ")\n" << "F (" << F.dot_.x_ << ";" << F.dot_.y_ << ")\n" << std :: endl; */
+    /* point F;
+    F = A;
+    std :: cout << "\nA (" << A.dot_.x_ << ";" << A.dot_.y_ << ")\nF (" << F.dot_.x_ << ";" << F.dot_.y_ << ")\n" << std :: endl; */
     
-    closed_polyline closed_line(line);
-    std :: cout << "\nclosed line lenght: "<< closed_line.length_polyline() << std :: endl;
+    /* closed_polyline closed_line(line);
+    std :: cout << "\nclosed line lenght: "<< closed_line.length_polyline() << std :: endl; */
 
     /* closed_polyline closed_line2(3, A.dot_, B.dot_, C.dot_);
     std :: cout << closed_line2.length_polyline() << std :: endl; */
+
+    polygon ABCD(line);
+    std :: cout << "\nperimeter: " << ABCD.length_polyline() << std :: endl;
+    std :: cout << "\narea: " << ABCD.polygon_area() << std :: endl;
 }
