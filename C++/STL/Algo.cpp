70203d4305cbb2c4b0aef79c52236ccf70063e2c
Ella Sleptsova <sl.ella@yandex.ru>
2022-04-15 11:11:54 +0300

    update algo

--- C++/STL/Algo.cpp
+++ C++/STL/Algo.cpp
@@ -40,45 +40,52 @@ int main(){
     algo.push_back(3);
     algo.push_back(6);
 
+    std::cout << "\n";
     for (int i = 0; i < algo.size(); ++i){
         std::cout << algo[i] << " ";
     }
 
-    std::cout << "\n\nall_of:\t\t" << my_all_of(algo.begin(), algo.end(), greater_1) << std::endl;
-    std::cout << "all_of:\t\t" << my_all_of(algo.begin(), algo.end(), less_1) << std::endl;
+    std::cout << "\n\nall_of(greater_1):\t\t" << my_all_of(algo.begin(), algo.end(), greater_1) << std::endl;
+    std::cout << "all_of(less_1):\t\t\t" << my_all_of(algo.begin(), algo.end(), less_1) << std::endl;
     std::cout << "\n";
 
-    std::cout << "any_of:\t\t" << my_any_of(algo.begin(), algo.end(), greater_4) << std::endl;
-    std::cout << "any_of:\t\t" << my_any_of(algo.begin(), algo.end(), less_1) << std::endl;
+    std::cout << "any_of(greater_4):\t\t" << my_any_of(algo.begin(), algo.end(), greater_4) << std::endl;
+    std::cout << "any_of(less_1):\t\t\t" << my_any_of(algo.begin(), algo.end(), less_1) << std::endl;
     std::cout << "\n";
 
-    std::cout << "none_of:\t" << my_none_of(algo.begin(), algo.end(), less_1) << std::endl;
-    std::cout << "none_of:\t" << my_none_of(algo.begin(), algo.end(), greater_4) << std::endl;
+    std::cout << "none_of(less_1):\t\t" << my_none_of(algo.begin(), algo.end(), less_1) << std::endl;
+    std::cout << "none_of(greater_4):\t\t" << my_none_of(algo.begin(), algo.end(), greater_4) << std::endl;
     std::cout << "\n";
 
-    std::cout << "one_of:\t\t" << my_one_of(algo.begin(), algo.end(), less_1) << std::endl;
-    std::cout << "one_of:\t\t" << my_one_of(algo.begin(), algo.end(), greater_5) << std::endl;
+    std::cout << "one_of(less_1):\t\t\t" << my_one_of(algo.begin(), algo.end(), less_1) << std::endl;
+    std::cout << "one_of(greater_5):\t\t" << my_one_of(algo.begin(), algo.end(), greater_5) << std::endl;
     std::cout << "\n";
 
-    std::cout << "is_sorted:\t" << my_is_sorted(algo.begin(), algo.end(), my_greater) << std::endl;
-    std::cout << "is_sorted:\t" << my_is_sorted(algo.begin(), algo.end(), my_low) << std::endl;
+    std::cout << "is_sorted(greater):\t\t" << my_is_sorted(algo.begin(), algo.end(), my_greater) << std::endl;
+    std::cout << "is_sorted(low):\t\t\t" << my_is_sorted(algo.begin(), algo.end(), my_low) << std::endl;
     std::cout << "\n";
 
-    std::cout << "is_partitioned:\t" << my_is_partitioned(algo.begin(), algo.end(), less_4) << std::endl;
-    std::cout << "is_partitioned:\t" << my_is_partitioned(algo.begin(), algo.end(), greater_1) << std::endl;
+    std::cout << "is_partitioned(less_4):\t\t" << my_is_partitioned(algo.begin(), algo.end(), less_4) << std::endl;
+    std::cout << "is_partitioned(greater_1):\t" << my_is_partitioned(algo.begin(), algo.end(), greater_1) << std::endl;
     std::cout << "\n";
 
-    std::cout << "find_not:\t" << *my_find_not(algo.begin(), algo.end(), 4) << std::endl;
-    std::cout << "find_not:\t" << *my_find_not(algo.begin(), algo.end(), 2) << std::endl;
+    std::cout << "find_not(4):\t\t\t" << *my_find_not(algo.begin(), algo.end(), 4) << std::endl;
+    std::cout << "find_not(2):\t\t\t" << *my_find_not(algo.begin(), algo.end(), 2) << std::endl;
     std::cout << "\n";
 
-    std::cout << "find_backward:\t" << *my_find_backward(algo.begin(), algo.end(), 2) << std::endl;
-    std::cout << "find_backward:\t" << *my_find_backward(algo.begin(), algo.end(), 5) << std::endl;
+    std::cout << "find_backward(2):\t\t" << *my_find_backward(algo.begin(), algo.end(), 2) << std::endl;
+    std::cout << "find_backward(5):\t\t" << *my_find_backward(algo.begin(), algo.end(), 5) << std::endl;
     std::cout << "\n";
 
-    std::cout << "is_palindrome:\t" << my_is_palindrome(algo.begin(), algo.end()) << std::endl;
+    for (int i = 0; i < algo.size(); ++i){
+        std::cout << algo[i] << " ";
+    }
+    std::cout << "\nis_palindrome:\t\t\t" << my_is_palindrome(algo.begin(), algo.end()) << std::endl;
     algo.push_back(3);
     algo.push_back(2);
-    std::cout << "is_palindrome:\t" << my_is_palindrome(algo.begin(), algo.end()) << std::endl;
+    for (int i = 0; i < algo.size(); ++i){
+        std::cout << algo[i] << " ";
+    }
+    std::cout << "\nis_palindrome:\t\t\t" << my_is_palindrome(algo.begin(), algo.end()) << std::endl;
     std::cout << "\n";
 }
--- C++/STL/Algo.hpp
+++ C++/STL/Algo.hpp
@@ -52,11 +52,11 @@ template <typename iterator, typename pred>
     template <typename iterator, typename pred>
     bool my_is_partitioned (iterator begin, iterator end, pred predicant){
         int count = 0;
-        int rez = predicant(*(begin));
+        int rez = predicant(*begin);
         for (; begin != end; ++begin){
-            if (rez != (predicant(*(begin)))){
+            if (rez != (predicant(*begin))){
                 ++count;
-                rez = predicant(*(begin));
+                rez = predicant(*begin);
             }
         }
         if (count == 1)
@@ -68,25 +68,34 @@ template <typename iterator, typename pred>
     template <typename iterator, typename pred>
     iterator my_find_not (iterator begin, iterator end, pred predicant){
         for ( ; begin != end; ++begin){
-            if (*(begin) != predicant)
+            if (*begin != predicant)
                 return begin;
         }
+        if (*begin == predicant)
+            return begin;
         return end + 1;
     }
 
     template <typename iterator, typename pred>
     iterator my_find_backward(iterator begin, iterator end, pred predicant){
         for ( ; end != begin; --end)
-            if (*(end) == predicant)
+            if (*end == predicant){
                 return end;
+            }
+        if (*end == predicant){
+            return end;
+        }
         return begin - 1;
     }
 
     template <typename iterator>
     bool my_is_palindrome(iterator begin, iterator end){
+        --end;
         for ( ; begin != end; ++begin, --end){
-            if (*(begin) != *(end))
+            if (*begin != *end)
                 return false;
         }
+        if (*begin != *end)
+            return false;
         return true;
     } 
