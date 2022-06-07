7bf22dfa064fa3533f8dfabc1ef8450ac811c611
Ella Sleptsova <sl.ella@yandex.ru>
2022-06-07 12:38:35 +0300

    Update Allocator.cpp

--- C++/Allocator/Allocator.cpp
+++ C++/Allocator/Allocator.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <list>
 #include <cstdarg>
+#include <iterator>
+#include <ctime>
+#include <ratio>
+#include <chrono>
 #include <exception>
 
 template <typename T>
@@ -23,7 +27,30 @@ public:
     typedef const T& const_reference;
     typedef T value_type;
 
-    my_alloc() = default;
+    my_alloc(){
+        block temp_block;
+        temp_block.size_ = 8;
+        temp_block.pointer_ = (pointer)malloc(sizeof(value_type) * temp_block.size_);
+        if (temp_block.pointer_ == nullptr){
+            throw std::bad_alloc();
+        }
+        mem_blocks.push_back(temp_block);
+        
+        temp_block.size_ = 4;
+        temp_block.pointer_ = (pointer)malloc(sizeof(value_type) * temp_block.size_);
+        if (temp_block.pointer_ == nullptr){
+            throw std::bad_alloc();
+        }
+        mem_blocks.push_back(temp_block);
+
+        temp_block.size_ = 8;
+        temp_block.pointer_ = (pointer)malloc(sizeof(value_type) * temp_block.size_);
+        if (temp_block.pointer_ == nullptr){
+            throw std::bad_alloc();
+        }
+        mem_blocks.push_back(temp_block);
+        std::cout << "\nCreate";
+    }
 
     my_alloc(unsigned int count, ...){
         va_list ap;
@@ -31,10 +58,14 @@ public:
         for (int i = 0; i < count; ++i){
             block temp_block;
             temp_block.size_ = va_arg(ap, unsigned int);
-            temp_block.pointer_ = malloc(sizeof(value_type) * temp_block.size_);
+            temp_block.pointer_ = (pointer)malloc(sizeof(value_type) * temp_block.size_);
+            if (temp_block.pointer_ == nullptr){
+                throw std::bad_alloc();
+            }
+            mem_blocks.push_back(temp_block);
         }
         va_end(ap);
-        std::cout << "\nAllocate";
+        std::cout << "\nCreate";
     }
 
     my_alloc(const my_alloc& other_alloc){
@@ -45,37 +76,54 @@ public:
             temp_block.used_ = i.used_;
             this->mem_blocks.push_back(temp_block);
         }
+        std::cout << "\nCreate";
     }
 
-    pointer allocate (size_type size){ // тут нужно что-то придуать с итераторами и блоками...
-        block i = mem_blocks.begin();
-        while (i != mem_blocks.end() && i.size_ < size){
+    pointer allocate (size_type size){ 
+        typename std::list<block>::iterator i = mem_blocks.begin();
+        while (i != mem_blocks.end() && (*i).size_ < size){
             ++i;
         }
-        if (i == mem_blocks.end() && i.size_ < size){
+        if (i == mem_blocks.end()){
             throw std::bad_alloc();
         }
-        while (i != mem_blocks.end() && i.used_ == true){
+        while (i != mem_blocks.end() && (*i).used_ == true){
             ++i;
         }
-        if (i == mem_blocks.end() && i.used_ == true){
+        if (i == mem_blocks.end() && (*i).used_ == true){
             throw std::bad_alloc();
         }
-        i.used_ = true;
-        return i.pointer_;
+        (*i).used_ = true;
+        std::cout << "\nAllocate";
+        return (*i).pointer_;
     }
 
-    void deallocate(pointer p, size_type n){ // тут нужно что-то придуать с итераторами и блоками...
-        block i = mem_blocks.begin();
-        while (i != mem_blocks.end() && i.pointer_ != p){
+    void deallocate(pointer p, size_type n){ 
+        typename std::list<block>::iterator i = mem_blocks.begin();
+        while (i != mem_blocks.end() && (*i).pointer_ != p){
             ++i;
         }
-        i.used_ = false;
+        (*i).used_ = false;
         std::cout << "\nDeallocate";
     }
 };
 
 int main(){
+    //auto start = std::chrono::high_resolution_clock::now();
     //my_alloc<int> Allocator(2, 2, 4, 8);
-    std::vector<int, my_alloc<int> > vec(2);
+    std::vector<int, my_alloc<int> > vec(2, 4);
+    std::cout << "\n";
+    std::cout << "\n" << vec[0];
+    std::cout << "\n" << vec[1];
+    std::cout << "\n";
+
+    vec.resize(5);
+    std::cout << "\n";
+    std::cout << "\n" << vec[0];
+    std::cout << "\n" << vec[1];
+    std::cout << "\n";
+
+    //auto end = std::chrono::high_resolution_clock::now();
+    //std::chrono::duration<float> duration = end - start;
+    //std::cout << "\n" << duration.count() << "s\n";
 }
